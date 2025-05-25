b2bb6b4ee382c897d72a3c8f1d54d3a3df252842
Luke-1310 <priviteraluca2001@gmail.com>
2025-05-25 19:04:20 +0200

    .

--- Progetto Tesi Privitera/q_learning_training.py
+++ Progetto Tesi Privitera/q_learning_training.py
@@ -101,7 +101,7 @@ def train_agent(env, font):
         evaluate_agent(env, font)
 
     if show_yes_no_dialog(env.screen, font, "Vuoi salvare la Q-table?"):
-        filename = f'q_table_{env.map_name}.npy'
+        filename = f"{'q_table_{env.map_name}.npy'}"
         full_path_q_table = f"{"Progetto Tesi Privitera/q_tables"}/{filename}"  
 
         np.save(full_path_q_table, env.q_values)
