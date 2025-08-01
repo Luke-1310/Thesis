c9dd7ac6d8d88fa4583ef47ef71ab4cc2b4ba931
Luke-1310 <priviteraluca2001@gmail.com>
2025-08-01 17:12:44 +0200

    piccola struttura iniziale

--- Progetto Tesi Privitera/traffic_rules_training.py
+++ Progetto Tesi Privitera/traffic_rules_training.py
@@ -8,3 +8,34 @@ def train_traffic_rules(env, font):
 
     #PRIMA LO CALCOLO E POI LUI SI ALLENA dopo averlo calcolato
 
+    #Parametri
+    epsilon = 1 
+    discount_factor = 0.9 
+    learning_rate = 0.1 
+    num_episodes = getattr(env, 'num_episodes', 2000)
+    episode_data = []
+    
+    #Loop di allenamento
+    for episode in range(num_episodes):
+        env.reset_game()  #DA FARE
+
+        while not is_route_completed(env, current_route): #Condizione di stop
+
+            #azioni
+            action_index = env.get_next_action_index(epsilon)  #Scegli la prossima azione DA FARE
+            old_position = env.agent_position[:]
+
+            #movimento
+            is_valid = env.get_next_action(action_index)  #Come si muove l'agente?
+
+            #reward
+            if is_valid:
+                reward = calculate_traffic_reward(env, old_position, action_index)  #Calcolo reward
+            else:
+                reward = -10  #Penalità per azione non valida
+
+            #q-learning update
+            old_q_value = env.q_values[old_position[1], old_position[0], old_car_in_vision, action_index]
+            temporal_difference = reward + (discount_factor * np.max(env.q_values[env.agent_position[1], env.agent_position[0], int(env.car_in_vision)])) - old_q_value
+            new_q_value = old_q_value + (learning_rate * temporal_difference)
+            env.q_values[old_position[1], old_position[0], old_car_in_vision, action_index] = new_q_value
